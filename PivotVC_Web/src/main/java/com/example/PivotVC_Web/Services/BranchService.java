b4a03b6d149a76332e0f0e52e079d32378b52f52
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-03 15:28:15 +0530

    Checkout and createBranch features added

--- PivotVC_Web/src/main/java/com/example/PivotVC_Web/Services/BranchService.java
+++ PivotVC_Web/src/main/java/com/example/PivotVC_Web/Services/BranchService.java
@@ -1,4 +1,44 @@
 package com.example.PivotVC_Web.Services;
 
+import com.example.PivotVC_Web.Entities.Branch;
+import com.example.PivotVC_Web.Entities.GitRepository;
+import com.example.PivotVC_Web.Entities.Head;
+import com.example.PivotVC_Web.Entities.RefType;
+import com.example.PivotVC_Web.Repository.BranchRepository;
+import com.example.PivotVC_Web.Repository.HeadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BranchService {
+    @Autowired
+    HeadRepository headRepository;
+    @Autowired
+    BranchRepository branchRepository;
+    public void createBranch(GitRepository repo,String branchName){
+        //we need to find the current commit
+        String currCommit;
+        Head head=headRepository.findByRepoId(repo.getId());
+        if(head.getRefType()== RefType.DETACHED){
+            currCommit=head.getCommitSha();
+        }
+        else{
+            Branch branch=branchRepository.findByBranchName(head.getBranchName());
+            currCommit=branch.getHeadCommitSha();
+        }
+        Branch branch=new Branch(repo,branchName,currCommit);
+        branchRepository.save(branch);
+    }
+    public void checkOut(String branchName,GitRepository gitRepository){
+        Branch branch=branchRepository.findByBranchName(branchName);
+        //we need to update headd
+        Head head=headRepository.findByRepoId(gitRepository.getId());
+        if(head.getRefType()==RefType.DETACHED){
+            head.setRefType(RefType.BRANCH);
+            head.setBranchName(branchName);
+            headRepository.save(head);
+        }
+        else{
+            head.setBranchName(branchName);
+            headRepository.save(head);
+        }
+    }
 }
