61f19e2fca4dcd5c74a41dfde8693d7408f3cc2b
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-05 15:59:12 +0530

    CORS added in CommitController

--- PivotVC_Web/src/main/java/com/example/PivotVC_Web/Controller/CommitController.java
+++ PivotVC_Web/src/main/java/com/example/PivotVC_Web/Controller/CommitController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/repo")
 public class CommitController {
