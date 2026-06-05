fa2763890318809fb8b15c08286cb8da3aaee246
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-05 15:15:01 +0530

    CORS added in agentController

--- PivotVC_Web/src/main/java/com/example/PivotVC_Web/Controller/AgentController.java
+++ PivotVC_Web/src/main/java/com/example/PivotVC_Web/Controller/AgentController.java
@@ -3,11 +3,9 @@ package com.example.PivotVC_Web.Controller;
 import com.example.PivotVC_Web.Repository.AgentAnalysisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/repo")
 public class AgentController {
